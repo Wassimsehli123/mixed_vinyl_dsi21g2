f1fee6a59f465656dacb12c556648a5f8587b1d2
Faycel CHAOUA <faycelchaoua@gmail.com>
2024-02-27 15:06:57 +0100

    Added some Tiwggy goodness

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,18 +2,30 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
 
 
-class VinylController
+class VinylController extends AbstractController
 {
     #[Route("/")]
     function homepage(): Response
     {
-        return new Response("Bonjour <strong>DSI21G1</strong><br><img src='https://assets.website-files.com/60b9fdcaf8c317a1cfdb2bd9/613b3216be53c8701f5a5026_symfony_black_03.png' width='200'> !");
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        ];
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => $tracks,
+        ]);
     }
     #[Route("/browse/{slug}")]
     function browse(string $slug = null): Response
